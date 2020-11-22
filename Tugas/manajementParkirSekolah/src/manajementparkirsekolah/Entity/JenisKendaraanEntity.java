e32a92777eb77ebab8545d1b60c854dadf661895
yogi hardianto <72394805+yogi-hardianto@users.noreply.github.com>
2020-11-23 00:57:39 +0700

    Add files via upload

---
+++ Tugas/manajementParkirSekolah/src/manajementparkirsekolah/Entity/JenisKendaraanEntity.java
@@ -0,0 +1,29 @@
+package Entity;
+public class JenisKendaraanEntity{
+    private String jenis,nopol;
+
+    public JenisKendaraanEntity(String jenis, String nopol) {
+        this.jenis = jenis;
+        this.nopol = nopol;          
+    }
+
+    public String getJenis() {
+        return jenis;
+    }
+
+    public void setJenis(String jenis) {
+        this.jenis = jenis;
+    }
+    
+    public String getNopol() {
+        return nopol;
+    }
+
+    public void setNopol(String nopol) {
+        this.nopol = nopol;
+    }
+    
+    
+
+    
+}
